3ca9ee58c640618087f2fff0b714a3428e043d3a
KRISHNA PRIYA MOLAKALAPALLI <84462660+KrishnaPriyaMolakalapalli@users.noreply.github.com>
2024-08-17 01:58:26 -0500

    Cloud watch Alarms

--- Cloud_Watch.py
+++ Cloud_Watch.py
@@ -10,6 +10,7 @@ try:
         MetricName='Errors',
         Namespace='AWS/Lambda',
         Statistic='Sum',
+        ActionsEnabled=True,
         Period=300,
         EvaluationPeriods=1,
         Threshold=1,
@@ -26,6 +27,105 @@ try:
     print("CloudWatch Alarm for Lambda errors created successfully.")
 except Exception as e:
     print(f"Error creating CloudWatch alarm: {e}")
+
+
+# Lambda Throttle Alarm
+try:
+    cloudwatch_client.put_metric_alarm(
+        AlarmName='LambdaThrottleAlarm',
+        MetricName='Throttles',
+        Namespace='AWS/Lambda',
+        Statistic='Sum',
+        Period=300,
+        EvaluationPeriods=1,
+        Threshold=1,
+        ComparisonOperator='GreaterThanOrEqualToThreshold',
+        AlarmActions=["arn:aws:sns:us-east-2:024848463817:TransactionNotifications"],
+        AlarmDescription='Alarm when the Lambda function is throttled',
+        Dimensions=[
+            {
+                'Name': 'FunctionName',
+                'Value': 'ProcessTransactionFunction'
+            }
+        ]
+    )
+    print("Lambda Throttle Alarm created successfully.")
+except Exception as e:
+    print(f"Error creating CloudWatch alarm: {e}")
+
+# DynamoDB Read Capacity Alarm
+try:
+    cloudwatch_client.put_metric_alarm(
+        AlarmName='DynamoDBReadCapacityAlarm',
+        MetricName='ConsumedReadCapacityUnits',
+        Namespace='AWS/DynamoDB',
+        Statistic='Sum',
+        Period=300,
+        EvaluationPeriods=1,
+        Threshold=100,  
+        ComparisonOperator='GreaterThanOrEqualToThreshold',
+        AlarmActions=["arn:aws:sns:us-east-2:024848463817:TransactionNotifications"],
+        AlarmDescription='Alarm when DynamoDB read capacity exceeds threshold',
+        Dimensions=[
+            {
+                'Name': 'TableName',
+                'Value': 'Transactions'
+            }
+        ]
+    )
+    print("DynamoDB Read Capacity Alarm created successfully.")
+except Exception as e:
+    print(f"Error creating CloudWatch alarm: {e}")
+
+# DynamoDB Write Capacity Alarm
+try:
+    cloudwatch_client.put_metric_alarm(
+        AlarmName='DynamoDBWriteCapacityAlarm',
+        MetricName='ConsumedWriteCapacityUnits',
+        Namespace='AWS/DynamoDB',
+        Statistic='Sum',
+        Period=300,
+        EvaluationPeriods=1,
+        Threshold=100,  
+        ComparisonOperator='GreaterThanOrEqualToThreshold',
+        AlarmActions=["arn:aws:sns:us-east-2:024848463817:TransactionNotifications"],
+        AlarmDescription='Alarm when DynamoDB write capacity exceeds threshold',
+        Dimensions=[
+            {
+                'Name': 'TableName',
+                'Value': 'Transactions'
+            }
+        ]
+    )
+    print("DynamoDB Write Capacity Alarm created successfully.")
+except Exception as e:
+    print(f"Error creating CloudWatch alarm: {e}")
+
+
+# SQS Approximate Number of Messages Alarm
+try:
+    cloudwatch_client.put_metric_alarm(
+        AlarmName='SQSApproximateNumberOfMessagesAlarm',
+        MetricName='ApproximateNumberOfMessagesVisible',
+        Namespace='AWS/SQS',
+        Statistic='Average',
+        Period=300,
+        EvaluationPeriods=1,
+        Threshold=10,  # Adjust based on acceptable message queue size
+        ComparisonOperator='GreaterThanOrEqualToThreshold',
+        AlarmActions=["arn:aws:sns:us-east-2:024848463817:TransactionNotifications"],
+        AlarmDescription='Alarm when there are too many messages in the SQS queue',
+        Dimensions=[
+            {
+                'Name': 'QueueName',
+                'Value': 'EcommerceTransactionQueue'
+            }
+        ]
+    )
+    print("SQS Approximate Number of Messages Alarm created successfully.")
+except Exception as e:
+    print(f"Error creating CloudWatch alarm: {e}")
+
 
 
 
